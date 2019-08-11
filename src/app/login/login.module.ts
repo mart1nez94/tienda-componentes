6265a268c590d7c0f61b8b3c1ba105879f78c109
mart1nez94 <armandoomtzz@gmail.com>
2019-08-11 16:09:53 -0500

    generate account component in login module

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-
+/* Componentes del Modulo Login */
+// Account Component -- Acceder al sistema
+import { AccountComponent } from './account/account.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [AccountComponent],
   imports: [
     CommonModule
   ]
